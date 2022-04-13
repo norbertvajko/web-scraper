db57020833280858e07d028a7a03c61aa0a628dd
Norbi <norbertvajko@yahoo.com>
2022-04-14 00:03:04 +0300

    update

--- Contact.php
+++ Contact.php
@@ -64,10 +64,36 @@
 
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <footer>
-    <div class="footer-main">
-        <p>Author:Vajko Norbert</p>
-        <p><a href="mailto:hege@example.com">norbertvajko@yahoo.com</a></p>
-        <p><a href="Contact.php">Contact</a></p>
+    <div class="footer-container">
+        <div class="about-section">
+            <h3>Little description</h3>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            <ul class="social-links">
+                <li><a href="#"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+        <div class="util-links-section">
+            <h3>Page links</h3>
+            <ul class="util-links-ref">
+                <li><a href="Contact.php">Contact</a></li>
+                <li><a href="Contact.php">Contact1</a></li>
+                <li><a href="Contact.php">Contact2</a></li>
+                <li><a href="Contact.php">Contact3</a></li>
+            </ul>
+        </div>
+        <div class="contact-section">
+            <h3>Contact Info</h3>
+            <ul class="contact-info">
+                <li><i class="fa fa-map-marker" aria-hidden="true"><a href="https://www.google.com/maps/place/Strada+Nicolae+B%C4%83lcescu+22,+Arad/@46.1703727,21.3171513,17z/data=!3m1!4b1!4m5!3m4!1s0x474599016319a9b3:0xc7f128d36aedf597!8m2!3d46.170369!4d21.31934" target="_blank">Strada Nicolae Bălcescu 22, Arad</a></i> </li>
+                <li><i class="fa fa-phone" aria-hidden="true"><a href="tel:123-456-7890">070-000-0000</a></i></li>
+                <li><i class="fa fa-envelope" aria-hidden="true"><a href="mailto:aaaa@email.com">norbertvajko@yahoo.com</a></i> </li>
+            </ul>
+        </div>
+    </div>
+    <div class="copyright-section">
+        <p>Copyright © 2022 Scrappy. All rights reserved.</p>
     </div>
 </footer>
 
