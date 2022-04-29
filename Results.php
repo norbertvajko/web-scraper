b413fdcd14f27409738511028d269505b6c4f863
Norbi <norbertvajko@yahoo.com>
2022-04-29 14:28:47 +0300

    update

--- Results.php
+++ Results.php
@@ -66,6 +66,11 @@
                     <a class="nav-link" href="Contact.php">Contact</a>
                 </li>
             </ul>
+            <div class="form-outline">
+                <input type="text" id="form12" class="form-control" />
+                <label class="form-label" for="form12">Example label</label>
+            </div>
+
             <ul class="navbar-nav d-flex flex-row ms-auto">
                 <li class="nav-item me-3 me-lg-0">
                     <a class="nav-link log-in" href="#" role="button">Log In</a>
@@ -101,6 +106,7 @@
 </nav>
 <!--------------------------------------------- End Nav Bar Area ------------------------------------------------------>
 
+<!--------------------------------------------- Start Banner Area ----------------------------------------------------->
 <header class="banner-results mt-100">
     <div class="container p-3">
         <div class="box position-relative">
@@ -127,7 +133,7 @@
 
                 <div class="user-functions ">
                     <div class="product-description">
-                        <button class="btn btn-primary  ">Description</button>
+                        <button class="btn btn-primary">Description</button>
                     </div>
                     <div class="product-gallery">
                         <button class="btn btn-primary">Photo Gallery</button>
@@ -170,6 +176,252 @@
         </div>
     </div>
 </header>
+<!----------------------------------------------- End Banner Area ----------------------------------------------------->
+
+<!----------------------------------------------- Start Main Area ----------------------------------------------------->
+
+<main class="main-results">
+
+    <section class="offers">
+        <section class="tab-menu">
+            <div class="container">
+                <div class="row mt-3">
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active" id="bestOfferTab" data-bs-toggle="tab"
+                                    data-bs-target="#home"
+                                    type="button" role="tab" aria-controls="home" aria-selected="true">Best offer
+                            </button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="descriptionT" data-bs-toggle="tab" data-bs-target="#profile"
+                                    type="button" role="tab" aria-controls="profile" aria-selected="false">Description
+                            </button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="statisticT" data-bs-toggle="tab" data-bs-target="#contact"
+                                    type="button" role="tab" aria-controls="contact" aria-selected="false">Statistics
+                            </button>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="bestOfferTab" role="tabpanel"
+                             aria-labelledby="home-tab">
+                            <div class="container">
+                                <h2>Oferte evidentiate</h2>
+                                <div class="row">
+                                    <div class="col best-offer-menu">
+                                        <div class="offer-company-logo">
+                                            <img src="https://p1.akcdn.net/partnerlogosmall/191379.jpg"
+                                                 alt="company-logo">
+                                        </div>
+                                    </div>
+                                    <div class="col best-offer-menu">
+                                        <div class="offer-rating">
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star-o"></i>
+                                        </div>
+                                        <span>(13 reviews)</span>
+                                    </div>
+                                    <div class="col best-offer-menu">
+                                        <div class="offer-price">
+                            <span class="best-offer-price">
+                                3 810,00 RON
+                            </span>
+                                        </div>
+                                        <span class="best-offer-stock">
+                                In stock
+                            </span>
+                                    </div>
+                                    <div class="col best-offer-menu">
+                                        <div class="best-to-shop">
+                                            <button class="btn btn-primary">
+                                                to shop
+                                                <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col best-offer-menu">
+                                        <div class="offer-company-logo">
+                                            <img src="https://p1.akcdn.net/partnerlogosmall/36258.jpg"
+                                                 alt="company-logo">
+                                        </div>
+                                    </div>
+                                    <div class="col best-offer-menu">
+                                        <div class="offer-rating">
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star-o"></i>
+                                        </div>
+                                        <span>(13 reviews)</span>
+                                    </div>
+                                    <div class="col best-offer-menu">
+                                        <div class="offer-price">
+                            <span class="best-offer-price">
+                                 4 429,99 RON
+                            </span>
+                                        </div>
+                                        <span class="best-offer-stock">
+                                In stock
+                            </span>
+                                    </div>
+                                    <div class="col best-offer-menu">
+                                        <div class="best-to-shop">
+                                            <button class="btn btn-primary">
+                                                to shop
+                                                <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col best-offer-menu">
+                                        <div class="offer-company-logo">
+                                            <img src="https://p1.akcdn.net/partnerlogosmall/165642.jpg"
+                                                 alt="company-logo">
+                                        </div>
+                                    </div>
+                                    <div class="col best-offer-menu">
+                                        <div class="offer-rating">
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star-o"></i>
+                                        </div>
+                                        <span>(13 reviews)</span>
+                                    </div>
+                                    <div class="col best-offer-menu">
+                                        <div class="offer-price">
+                            <span class="best-offer-price">
+                                5 088,99 RON
+                            </span>
+                                        </div>
+                                        <span class="best-offer-stock">
+                                In stock
+                            </span>
+                                    </div>
+                                    <div class="col best-offer-menu">
+                                        <div class="best-to-shop">
+                                            <button class="btn btn-primary">
+                                                to shop
+                                                <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="descriptionTab">...</div>
+                    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="statisticsTab">...</div>
+                </div>
+            </div>
+        </section>
+
+        <section class="rest-offers">
+            <div class="container">
+                <div class="row">
+                    <h3>Restul ofertelor</h3>
+                    <div class="rest-offers-content">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </section>
+</main>
+
+<!------------------------------------------------- End Main Area ----------------------------------------------------->
+
+<!----------------------------------------------- Start Footer Area --------------------------------------------------->
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 about-section">
+                <h3>Little description</h3>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                    dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum."</p>
+                <ul class="social-links d-flex ps-0">
+                    <li><a href="https://github.com/norbertvajko" target="_blank"><i class="fa fa-github"
+                                                                                     aria-hidden="true"></i></a>
+                    </li>
+                    <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-6 text-end contact-section text-sm-center col text-center">
+                <h3>Contact Info</h3>
+                <ul class="contact-info">
+                    <li><i class="fa fa-map-marker" aria-hidden="true"><a
+                                    href="https://www.google.com/maps/place/Strada+Nicolae+B%C4%83lcescu+22,+Arad/@46.1703727,21.3171513,17z/data=!3m1!4b1!4m5!3m4!1s0x474599016319a9b3:0xc7f128d36aedf597!8m2!3d46.170369!4d21.31934"
+                                    target="_blank">Strada Nicolae Bălcescu 22, Arad</a></i></li>
+                    <li><i class="fa fa-phone" aria-hidden="true"><a href="tel:123-456-7890">070-000-0000</a></i></li>
+                    <li><i class="fa fa-envelope" aria-hidden="true"><a
+                                    href="mailto:aaaa@email.com">norbertvajko@yahoo.com</a></i></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="newsletter-area container  my-5">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="newsletter-header m-auto text-center">
+                        <h5>Do you want to receive smart shopping advice from us?</h5>
+                        <p>Then subscribe to our newsletter!</p>
+                    </div>
+                    <form class="newsletter-form">
+                        <div class="d-flex justify-content-center flex-column flex-md-row w-50 align-items-center m-auto">
+                            <label>
+                                <input type="email" class="form-control mt-3 " placeholder="Enter e-mail">
+                            </label>
+                            <a href="#" class="subscribe-button position-relative d-flex flex-wrap ms-3 mt-3">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                Subscribe
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="container text-center copyright-section">
+            <p class="m-0">Copyright © 2022 Scrappy. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+<!------------------------------------------------ End Footer Area ---------------------------------------------------->
+<script>
+    function myFunction() {
+        const searchButton = document.getElementById('search-button');
+        const searchInput = document.getElementById('search-input');
+        searchButton.addEventListener('click', () => {
+            const inputValue = searchInput.value;
+            alert(inputValue);
+        });
+    }
+</script>
 
 </body>
 </html>
