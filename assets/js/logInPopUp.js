4b0e3d6886ccb3ba95ffcc11f32e5dcdebbfa187
Norbi <norbertvajko@yahoo.com>
2022-05-04 23:21:21 +0300

    update

--- assets/js/logInPopUp.js
+++ assets/js/logInPopUp.js
@@ -4,3 +4,4 @@ document.querySelector("#showLogin").addEventListener("click",function() {
 document.querySelector(".popup .close-btn").addEventListener("click",function() {
     document.querySelector(".popup").classList.remove("active");
 });
+
---
+++ views/functionality/connDB.php
@@ -0,0 +1,11 @@
+<?php
+
+$uname = "n_vajko";
+$dbPass = "PQqNVaYk9V9ZWet";
+$host = "internship.rankingcoach.com";
+$db = "n_vajko";
+
+$conn = mysqli_connect("$host", '$uname' , '$dbPass' , '$db')
+               or die ("DB Connection Error");
+?>
+
