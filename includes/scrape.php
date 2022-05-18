30fa635c3d2871e7879810033d0c092501f6b250
Norbi <norbertvajko@yahoo.com>
2022-05-19 00:24:37 +0300

    update

--- includes/scrape.php
+++ includes/scrape.php
@@ -1,47 +1,115 @@
 <?php
 
 include "connDB.php";
+//
+//$flancoProducts = [
+//    'name' => '!<h2>(.*?)<\/h2>!',
+//    'price' => '!<span class="special-price"><span class="price">(.*?)(?:,)<|<span class="singlePrice"><span class="price">(.*?)(?:,)<!',
+//    'rating' => '!<div class="rating-result" id="rating-result_.*?"><span><span>(.*?)(?:%)<\/span><\/span><\/div>!',
+//    'inStock' => '!<div class="produs-status">.*>(.*?)<\/span>  <\/div> <\/div>!',
+//    'images' => '!<span class=""><img class=""  src="(.*?)" .*"\/><\/span><\/span>!',
+//    'link' => '!<a class="product-item-link" href="(.*?)">!'
+//];
+//
+//for ($page = 1; $page < 13; $page++) { webCrawl('https://www.flanco.ro/telefoane-tablete/smartphone/p/' . $page. '.html', $flancoProducts); }
+//
+//$ideallProducts = [
+//    'name' => '!<a href="\/.*?\/.*" title=".*">(.*?)<\/a>!',
+//    'price' => '!<span class=".*?">(.*?)<sup>.*?<\/sup>.*?<\/span>!',
+//    'rating' => '!!',
+//    'inStock' => '!<span class="category-delivery-text">(.*?)<\/span>|<div class="category-delivery-container">
+//
+//.*<span class="">(.*?)<\/span>
+//
+//                        <\/div>!',
+//    'images' => '!<img src="(.*?)\?w=200&h=200" id=".*?"!',
+//    'link' => '!!'
+//];
+//for ($page = 1; $page <27; $page++) { webCrawl('https://www.ideall.ro/magazin-telefoane-mobile?pag='.$page , $ideallProducts); }
 
-function web_crawl($pageSt , $pageIndex ) {
-    for ($page = $pageSt; $page < $pageIndex; $page++) {
+//$celProducts = [
+//    'name' => '!class="productListing-data-b product_link product_name"><span >(.*?)<\/span>!'
+//];
+//webCrawl('https://www.cel.ro/telefoane-mobile/',$celProducts);
 
-        $result = curl_data(
-            [
-                '0' => [
-                    'option_name' => CURLOPT_URL,
-                    'option_value' => 'https://www.flanco.ro/telefoane-tablete/smartphone/p/' . $page . '.html'
-                ],
-                '1' => [
-                    'option_name' => CURLOPT_RETURNTRANSFER,
-                    'option_value' => true
-                ],
-            ]
-        );
+//$quickProducts = [
+//    'name' => '!<a\s\shref=".*?"\s>
+//.*?<div class="card-product-title" translate="no" style="white-space: nowrap; max-width: 200px; overflow: hidden;">
+//.*?.*?
+//.*?<\/div>
+//.*?<div class="card-product-description ">
+//.*?(.*?)
+//.*?<\/div>
+//.*?<\/a>!',
+//    'price' => '!<div class="priceFormat card-price" translate="no">(.*?)<sup>.*?<\/sup><sub>Lei<\/sub><\/div>!'
+//];
+//webCrawl('https://www.quickmobile.ro/telefoane-si-accesorii/telefoane-mobile', $quickProducts);
 
-        $regexName = '!<h2>(.*?)<\/h2>!';
-        $regexRating = '!<div class="rating-result" id="rating-result_.*?"><span><span>(.*?)(?:%)<\/span><\/span><\/div>!';
-        $regexStock = '!<div class="produs-status">.*>(.*?)<\/span>  <\/div> <\/div>!';
-        $regexImage = '!<span class=""><img class=""  src="(.*?)" .*"\/><\/span><\/span>!';
-        $regexPrice = '!<span class="special-price"><span class="price">(.*?)(?:,)<|<span class="singlePrice"><span class="price">(.*?)(?:,)<!';
+//$altexProducts = [
+//  'name' => '!<h2 class="Product-nameHeading leading-20 text-sm truncate-3-lines min-h-60px">(.*?)<\/h2>!',
+//    'price' => '!<span class="Price-int leading-none">(.*?)<\/span>!'
+//];
+//webCrawl('https://altex.ro/telefoane/cpl/',$altexProducts);
 
-        $phones = [
-            'name' => scrape_data($regexName, $result),
-            'rating' => scrape_data($regexRating, $result),
-            'inStock' => scrape_data($regexStock, $result),
-            'images' => scrape_data($regexImage, $result),
-            'price' => scrape_data($regexPrice, $result),
-//        'description' => scrape_data($regexDescription, $result),
-//        'link' => scrape_data(link, $result)
-        ];
+//$emagProducts = [
+//    'name' => '!class="card-v2-title semibold mrg-btm-xxs js-product-url" data-zone="title">(.*?)<\/a>!',
+//    'reviews' => '!<span class="average-rating semibold">(.*?)<\/span>!'
+//];
+//webCrawl('https://www.emag.ro/telefoane-mobile/c?tree_ref=13&ref=cat_tree_93',$emagProducts);
 
-        $obj = json_encode($phones);
-        $test = json_decode($obj);
+function webCrawl($url, $products)
+{
 
-        for ($i = 0; $i < count($test->name); $i++)
-            upload_data($test, $i);
+    $curlOptions = [
+        '0' => [
+            'option_name' => CURLOPT_URL,
+            'option_value' => $url
+        ],
+        '1' => [
+            'option_name' => CURLOPT_RETURNTRANSFER,
+            'option_value' => true
+        ],
+        '2' => [
+            'option_name' => CURLOPT_FAILONERROR,
+            'option_value' => true
+        ],
+        '3' => [
+            'option_name' => CURLOPT_USERAGENT,
+            'option_value' => 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.204 Safari/534.16'
+        ],
+        '4' => [
+            'option_name' => CURLOPT_FOLLOWLOCATION,
+            'option_value' => true
+        ],
+        '5' => [
+            'option_name' => CURLOPT_AUTOREFERER,
+            'option_value' => true
+        ],
+        '6' => [
+            'option_name' => CURLOPT_SSL_VERIFYPEER,
+            'option_value' => false
+        ],
+        '7' => [
+            'option_name' => CURLOPT_HEADER,
+            'option_value' => true
+        ]
+
+    ];
+
+    $result = curl_data($curlOptions);
+
+    foreach ($products as $key => $value) {
+        $products[$key] = scrape_data($value, $result);
     }
-}
+    print_r($products);
 
+    $obj = json_encode($products);
+    $test = json_decode($obj);
+
+//    for ($i = 0; $i < count($test->name); $i++) {
+//        upload_data($test, $i);
+//    }
+}
 function scrape_data($regex_stock, $result)
 {
     preg_match_all($regex_stock, $result, $match);
@@ -54,23 +122,22 @@ function scrape_data($regex_stock, $result)
 
     return $match[1];
 }
-
 function upload_data($phones, $index)
 {
     $phoneName = $phones->name[$index];
     $phoneReviews = $phones->rating[$index];
-    $phonePrice = $phones->price[$index];
+    $phonePrice = str_replace(',', '', $phones->price[$index]);
     $phoneInStock = $phones->inStock[$index];
     $phoneImages = $phones->images[$index];
+    $phoneLink = $phones->link[$index];
 
-    $query = "INSERT INTO products (shop_id, category_id, name, description, reviews, price, in_stock, images)
-  			  VALUES('4', '1', '$phoneName', 'fdsfsd', '$phoneReviews', '$phonePrice', '$phoneInStock', '$phoneImages')";
+    $query = "INSERT INTO products (shop_id, category_id, name, description, reviews, price, in_stock, images , link)
+  			  VALUES('4', '1', '$phoneName', 'fdsfsd', '$phoneReviews', '$phonePrice', '$phoneInStock', '$phoneImages', '$phoneLink')";
 
     if (!mysqli_query($GLOBALS['conn'], $query))
         echo mysqli_error($GLOBALS['conn']) . '<br>';
 
 }
-
 function curl_data($options)
 {
     $curl = curl_init();
@@ -78,10 +145,12 @@ function curl_data($options)
     for ($i = 0; $i < count($options); $i++) {
         curl_setopt($curl, $options[$i]['option_name'], $options[$i]['option_value']);
     }
+
+    if (!$curl) {
+        echo "Page not reached!";
+        exit();
+    }
     return curl_exec($curl);
 }
 
 
-
-
-web_crawl(1,5, );
--- includes/test.php
+++ includes/test.php
@@ -1,1 +1,76 @@
 <?php
+//
+//// Create a user agent so websites don't block you
+//$userAgent = 'Googlebot/2.1 (https://www.google.bot.com/bot.html)';
+//
+//// Create the initial link you want.
+//$target_url = 'https://www.emag.ro/';
+//
+//// Initialize curl and following options
+//$ch = curl_init();
+//curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+//curl_setopt($ch, CURLOPT_URL,$target_url);
+//curl_setopt($ch, CURLOPT_FAILONERROR, true);
+//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+//curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+//curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+//
+//
+//// Grab the html from the page
+//$html = curl_exec($ch);
+//
+//// Error handling
+//if(!$html){
+//    //handle error if page was not reachable, etc
+//    exit();
+//}
+//
+//
+//// Create a new DOM Document to handle scraping
+//$dom = new DOMDocument();
+//@$dom->loadHTML($html);
+//
+//// $product_list = $dom->getElementById()
+//
+//$h1s = $dom->getElementsByTagName('h2'); //returns DOMNodeList object
+//$h1_array = array();
+//
+//foreach($h1s as $h1) {
+//    $title_text = $h1->textContent;
+//    $h1_array[] = $title_text;
+//    echo $title_text . '<br>';
+//}
+//
+//// fix html5/svg errors
+//libxml_use_internal_errors(true);
+//
+//// $dom->loadHTML("<section></section>");
+//// $htmlNodes = $dom->getElementsByTagName('section');
+//
+//
+//// get your element, you can do this numerous ways like getting by tag, id or using a DOMXPath object
+//// This example gets elements with id forward-link which might be a div or ul or li, etc
+//// It then gets all the a tags (links) within all those divs, uls, etc
+//// Then it takes the first link in the array of links and then grabs the href from the link
+//// $search = $dom->getElementById('main-container');
+//// $forwardlink = $search->getElementsByTagName('a');
+//// $forwardlink = $forwardlink->item(0);
+//// // $forwardlink = $getNamedItem('');
+//// $href = $forwardlink->textContent;
+//
+//
+//// Now that you have the link you want to follow/click to
+//// Set the target_url for the cUrl to the new url
+//curl_setopt($ch, CURLOPT_URL, $target_url);
+//
+//$html = curl_exec($ch);
+//
+//
+//// do what you want with your new link!
+///
+///
+///
+
+$url = file_get_contents("https://www.flanco.ro/telefoane-tablete/smartphone/p/1.html");
+echo $url;
